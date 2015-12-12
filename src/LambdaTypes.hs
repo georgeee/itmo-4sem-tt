490378c0075e2c4b4f91d8256bd7a228c4b96eb1
georgeee <george.agapov@gmail.com>
2015-12-13 01:02:43 +0300

    TT: initial commit

---
+++ src/LambdaTypes.hs
@@ -0,0 +1,13 @@
+module LambdaTypes where
+
+type Var = String
+
+data UntypedLambda = ULVar { ulVarName :: Var }
+                   | ULAbs { ulVarName :: Var, ulExpr :: UntypedLambda }
+                   | ULApp { ulLeft :: UntypedLambda, ulRight :: UntypedLambda }
+
+
+instance Show UntypedLambda where
+  show (ULVar name) = name
+  show (ULAbs name e) = "(\\" ++ name ++ ". " ++ (show e) ++ ")"
+  show (ULApp l r) = "(" ++ (show l) ++ " " ++ (show r) ++ ")"
---
+++ src/Main.hs
@@ -0,0 +1,43 @@
+module Main where
+import Text.Parsec
+import System.Console.GetOpt
+import LambdaTypes
+import LambdaParser
+import Data.List
+import Control.Monad
+import Data.List
+import System.Environment
+import System.IO
+
+data Options = Options { optInput :: Maybe FilePath
+                       , optTask :: Int
+                       }
+
+defaultOptions = Options { optInput = Nothing
+                         , optTask = 0
+                         }
+
+
+options :: [ OptDescr (Options -> IO Options) ]
+options = [ Option "i" ["input"] (ReqArg (\f os -> return $ os { optInput = Just f }) "FILE") "Input file"
+          , Option "t" ["task"] (ReqArg (\f os -> return $ os { optTask = read f }) "INT") "Task id"
+          ]
+
+splitBy _ [] = []
+splitBy p xs = impl . span (not . p) $ xs
+  where impl ([], _) = []
+        impl (l, rest) = l : splitBy p (dropWhile p rest)
+
+multiline opts algo = case optInput opts of
+                   Just fname -> readFile fname
+                          >>= mapM algo . splitBy (== '\n')
+                          >>= mapM_ putStrLn
+                   Nothing -> forever $ getLine >>= algo >>= putStrLn
+
+main = do
+  args <- getArgs
+  let (actions, nonOptions, errors) = getOpt Permute options args
+  opts <- foldl (>>=) (return defaultOptions) actions
+
+  case optTask opts of
+    1 -> multiline opts $ return . either (("Error: " ++) . show) show . parse (ulParse) ""
