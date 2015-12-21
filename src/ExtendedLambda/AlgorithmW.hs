60b1e8f36318c2b00a687061e41860774411cf48
georgeee <george.agapov@gmail.com>
2015-12-22 00:26:12 +0300

    Working on ExtendedLambda...

---
+++ src/ExtendedLambda/AlgorithmW.hs
@@ -0,0 +1,12 @@
+module ExtendedLambda.AlgorithmW where
+
+import ExtendedLambda.Types
+import ExtendedLambda.Base
+import Unification
+
+typeBool = Const "Bool"
+typeInt = Const "Int"
+typeAnd = Function "&"
+typeOr = Function "|"
+
+
